9891ec7a612f30e06a8fccb8947a632e2512cd1b
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2026-02-23 20:36:40 +0530

    add write to file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+console.log('Read file');
+
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
+
+
+console.log('Write file');
+
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
+});
